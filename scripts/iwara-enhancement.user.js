5a24fc50d172ad2f11e20f8b38f2cd400e74d1b2
guansss <821143943@qq.com>
2021-09-02 01:40:58 +0800

    Iwara Enhancement v0.6

--- scripts/iwara-enhancement.user.js
+++ scripts/iwara-enhancement.user.js
@@ -2,7 +2,7 @@
 // @name         Iwara Enhancement
 // @name:zh-CN   Iwara增强
 // @namespace    https://github.com/guansss/userscripts
-// @version      0.5
+// @version      0.6
 // @description  Multiple UI enhancements for better experience.
 // @description:zh-CN 多种增强体验的界面优化
 // @author       guansss
