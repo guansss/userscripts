4487610c1054e83bb3107af25e00504b043fa178
Guan <821143943@qq.com>
2022-02-14 23:11:59 +0800

    Add Douyu script

---
+++ userscripts/douyu/index.ts
@@ -0,0 +1,22 @@
+import { ready } from '../../utils/events';
+
+ready.then(() => {
+    document.addEventListener('mouseleave', onLeave);
+});
+
+function onLeave(e: MouseEvent) {
+    if (e.target === document) {
+        const stuckComment = document.getElementById('comment-higher-container')?.firstElementChild;
+
+        if (stuckComment) {
+            document.dispatchEvent(new MouseEvent('mousemove'));
+        }
+    }
+}
+
+if (import.meta.hot) {
+    import.meta.hot.accept();
+    import.meta.hot.on('vite:beforeUpdate', () => {
+        document.removeEventListener('mouseleave', onLeave);
+    });
+}
