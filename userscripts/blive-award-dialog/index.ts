d740099ec0742a1a16b710db9a69b8fff0ef6d0b
Guan <821143943@qq.com>
2023-03-15 20:09:08 +0800

    feat(blive-award-dialog): new

---
+++ userscripts/blive-award-dialog/index.ts
@@ -0,0 +1,7 @@
+GM_addStyle(`
+#anchor-guest-box-id {
+  display: none;
+}
+`)
+
+export {}
---
+++ userscripts/blive-award-dialog/meta.ts
@@ -0,0 +1,8 @@
+import { ScriptMeta } from "../@common/meta"
+
+export default {
+  version: "0.1",
+  name: "Bilibili live - remove award dialog",
+  match: "*://live.bilibili.com/*",
+  noframes: true,
+} satisfies ScriptMeta
