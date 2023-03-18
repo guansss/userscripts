7f47a87d0ffb7eed81638c840943c6cd72904ca1
Guan <821143943@qq.com>
2023-03-19 02:31:32 +0800

    release(iwara enhancement): v1.0

--- userscripts/iwara-enhancement/meta.ts
+++ userscripts/iwara-enhancement/meta.ts
@@ -3,14 +3,14 @@ import rootMeta from "../meta"
 
 export default {
   ...rootMeta,
-  version: "0.11",
+  version: "1.0",
   name: {
     default: "Iwara Enhancement",
     "zh-CN": "Iwara增强",
   },
   updateURL:
     "https://sleazyfork.org/scripts/416003-iwara-enhancement/code/Iwara%20Enhancement.user.js",
-  match: "*://staging.iwara.tv/*",
+  match: "*://*.iwara.tv/*",
   runAt: "document-start",
   noframes: true,
 } satisfies ScriptMeta
