e1a6389625c25201c487fe6ccabb07daf3edf343
Guan <821143943@qq.com>
2023-03-22 02:42:29 +0800

    chore: update meta

--- userscripts/iwara-enhancement/meta.ts
+++ userscripts/iwara-enhancement/meta.ts
@@ -8,6 +8,10 @@ export default {
     default: "Iwara Enhancement",
     "zh-CN": "Iwara增强",
   },
+  description: {
+    default: "Please refer to the script's homepage for more information.",
+    "zh-CN": "请参考脚本的主页以获取更多信息",
+  },
   updateURL:
     "https://sleazyfork.org/scripts/416003-iwara-enhancement/code/Iwara%20Enhancement.user.js",
   match: "*://*.iwara.tv/*",
--- userscripts/meta.ts
+++ userscripts/meta.ts
@@ -1,7 +1,7 @@
 import { ScriptMeta } from "./@common/meta"
 
 export default {
-  namespace: "https://github.com/guansss",
+  namespace: "https://github.com/guansss/userscripts",
   source: "https://github.com/guansss/userscripts",
   supportURL: "https://github.com/guansss/userscripts/issues",
   author: "guansss",
