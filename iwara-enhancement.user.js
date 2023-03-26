116aef0f22dc564bd1a437082c117a3ff1d85e03
Guan <821143943@qq.com>
2023-03-26 19:53:18 +0800

    Updates

--- iwara-enhancement.user.js
+++ iwara-enhancement.user.js
@@ -373,6 +373,11 @@
           break
         }
   }
+
+  function cancelOnLeave(onLeave, promise) {
+    onLeave(() => promise.cancel())
+    return promise
+  }
   let reactEventHandlersKey = ""
 
   function getReactEventHandlers(element) {
@@ -383,271 +388,6 @@
         reactEventHandlersKey = key
         return element[key]
       }
-  }
-
-  // a partial structure of the video data defined in iwara's video page,
-  // including only the properties we need
-
-  const FILENAME_KEYWORDS = [
-    "ID",
-    "TITLE",
-    "RES",
-    "AUTHOR",
-    "DATE",
-    "UP_DATE",
-    "DATE_TS",
-    "UP_DATE_TS",
-  ]
-  const RESOLUTIONS = ["Source", "540p", "360p"]
-
-  const autoEnabled = (0, external_Vue_namespaceObject.ref)(storage.get("auto_down_enabled"))
-  const filenameTemplate = (0, external_Vue_namespaceObject.ref)(storage.get("filename"))
-  const illegalCharReplacement = (0, external_Vue_namespaceObject.ref)(
-    storage.get("illegal_char_replacement")
-  )
-  const resolution = (0, external_Vue_namespaceObject.ref)(storage.get("preferred_res"))
-
-  const videoInfo = (0, external_Vue_namespaceObject.reactive)({
-    id: "",
-    title: "",
-    author: "",
-    created: 0,
-    size: 0,
-    error: "",
-  })
-  const sources = (0, external_Vue_namespaceObject.reactive)([])
-  const source = (0, external_Vue_namespaceObject.computed)(
-    () =>
-      sources.find((_ref) => {
-        let { label } = _ref
-        return label === resolution.value
-      }) || sources[0]
-  )
-
-  // indicates whether the sources belong to current page
-  const hasFreshSources = (0, external_Vue_namespaceObject.ref)(false)
-
-  const filename = (0, external_Vue_namespaceObject.computed)(() => {
-    try {
-      if (!source.value) throw "Please open a video"
-
-      return resolveFilename(filenameTemplate.value, source.value)
-    } catch (e) {
-      return `Unable to resolve filename (${formatError(e)})`
-    }
-  })
-
-  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
-    storage.set("preferred_res", resolution.value)
-  )
-  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
-    storage.set("filename", filenameTemplate.value)
-  )
-  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
-    storage.set("auto_down_enabled", autoEnabled.value)
-  )
-  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
-    convertDownloadDropdown(void 0, autoEnabled.value, source.value)
-  )
-
-  function useDownloaderSettings() {
-    return {
-      FILENAME_KEYWORDS,
-      RESOLUTIONS,
-      autoDownEnabled: autoEnabled,
-      resolution,
-      filenameTemplate,
-      filenamePreview: filename,
-      illegalCharReplacement,
-    }
-  }
-
-  page("video", (pageID, onLeave) => {
-    const videoActions = $(".page-video__actions").get(0)
-
-    if (!videoActions) {
-      log("Could not find video actions.")
-      return
-    }
-
-    const actionsObserver = new SimpleMutationObserver((mutation) =>
-      mutation.addedNodes.forEach((node) => {
-        if (hasClass(node, "dropdown")) {
-          updateVideoInfo(videoActions)
-          updateSources(node)
-
-          if (autoEnabled.value) convertDownloadDropdown(node, true, source.value)
-        }
-      })
-    )
-    actionsObserver.observe(videoActions, { childList: true, immediate: true })
-
-    onLeave(() => {
-      // prevent unexpected downloads
-      hasFreshSources.value = false
-
-      actionsObserver.disconnect()
-    })
-  })
-
-  function updateVideoInfo(videoActions) {
-    try {
-      // FIXME: reading the prop by a path is quite unreliable, any improvement?
-      const video = getReactEventHandlers(videoActions).children[1].props.video
-
-      videoInfo.id = video.id
-      videoInfo.title = video.title
-      videoInfo.created = new Date(video.createdAt).getTime()
-      videoInfo.author = video.user.name
-      videoInfo.size = video.file.size
-    } catch (e) {
-      log(e)
-      videoInfo.error = e + ""
-    }
-  }
-
-  function updateSources(downloadDropdown) {
-    const newSources = $(downloadDropdown)
-      .find(".dropdown__content a")
-      .map(function () {
-        const url = this.href
-        const label = this.innerText
-
-        return { url, label }
-      })
-      .get()
-
-    if (!newSources.length) return
-
-    sources.splice(0, sources.length, ...newSources)
-
-    hasFreshSources.value = true
-  }
-
-  function convertDownloadDropdown(downloadDropdown, enabled, source) {
-    const $dropdown = downloadDropdown ? $(downloadDropdown) : $(".page-video__actions > .dropdown")
-    const $button = $dropdown.find(".downloadButton")
-    const rawButtonText = $button.text().replace(/\s*\(.*\)/, "")
-
-    if (!$dropdown.length || !$button.length) return
-
-    if (enabled) {
-      if (!$dropdown.data("converted"))
-        $dropdown
-          .data("converted", true)
-          .on("click", function () {
-            download(this)
-          })
-          .children(".dropdown__content")
-          .css("display", "none")
-
-      $button.text(rawButtonText + (source ? ` (${source.label})` : ""))
-    } else {
-      $dropdown
-        .data("converted", false)
-        .off("click")
-        .children(".dropdown__content")
-        .css("display", "")
-      $button.text(rawButtonText)
-    }
-  }
-
-  function download(downloadDropdown) {
-    try {
-      if (!hasFreshSources.value) throw new Error("No sources found in current page.")
-      if (!source.value) throw new Error("Missing source.")
-
-      const $downloadButton = $(downloadDropdown).find(".downloadButton")
-
-      const filename = resolveFilename(filenameTemplate.value, source.value)
-
-      log("Downloading:", filename, source.value.url, GM_info.downloadMode)
-
-      if ("browser" === GM_info.downloadMode) {
-        setDownloadButtonEnabled(false)
-
-        if (false);
-
-        GM_download({
-          url: source.value.url,
-          name: filename,
-          onload: () => downloadEnded("onload"),
-          onerror: (e) => downloadEnded("onerror", e),
-          ontimeout: () => downloadEnded("ontimeout"),
-        })
-      } else {
-        const a = document.createElement("a")
-        a.href = source.value.url
-        a.download = filename
-        a.click()
-      }
-
-      function setDownloadButtonEnabled(enabled) {
-        log(enabled)
-        if (enabled) {
-          // TODO: properly disable the button
-          $(downloadDropdown).css("pointer-events", "")
-          $downloadButton.css("background-color", "")
-        } else {
-          $(downloadDropdown).css("pointer-events", "none")
-          $downloadButton.css("background-color", "var(--primary-dark)")
-        }
-      }
-
-      function downloadEnded(type, e) {
-        setDownloadButtonEnabled(true)
-
-        if ("ontimeout" === type) e = { error: "timed_out" }
-
-        if (e && e.error) {
-          log(e)
-          printDownloadMessage(
-            `Download Error (${e.error}): ${(e.details && e.details.current) || "No info"}`
-          )
-        }
-      }
-    } catch (e) {
-      log(e)
-      printDownloadMessage(e + "")
-    }
-  }
-
-  function resolveFilename(template, source) {
-    if (videoInfo.error) throw new Error("Broken video info: " + videoInfo.error)
-
-    const replacements = {
-      ID: videoInfo.id,
-      TITLE: videoInfo.title,
-      RES: source.label,
-      AUTHOR: videoInfo.author,
-      DATE: formatDate(new Date(), ""),
-      DATE_TS: new Date().getTime() + "",
-      UP_DATE: formatDate(new Date(videoInfo.created), ""),
-      UP_DATE_TS: videoInfo.created + "",
-    }
-
-    const wrappedKeywords = FILENAME_KEYWORDS.map((k) => `{${k}}`)
-    const regex = new RegExp(`(${wrappedKeywords.join("|")})`, "g")
-
-    const basename = template.replace(regex, (match) => {
-      const keyword = match.slice(1, -1)
-      const value = replacements[keyword]
-
-      // remove path delimiters in keyword values
-      return value.replace(/[/\\]/g, illegalCharReplacement.value)
-    })
-
-    const ext = source.url.slice(source.url.lastIndexOf(".")).replace(/[^A-Za-z0-9.]/g, "")
-
-    const filename = basename + ext
-
-    return sanitizePath(filename, illegalCharReplacement.value)
-  }
-
-  function printDownloadMessage(msg) {
-    $(".page-video__bottom")
-      .css("flex-wrap", "wrap")
-      .append(`<div style="flex: 100% 0 0">${msg}</div>`)
   }
 
   // sometimes I just don't want the script to depend on Lodash...
@@ -722,6 +462,269 @@
       interval,
       cancelOnReload
     )
+  }
+
+  // a partial structure of the video data defined in iwara's video page,
+  // including only the properties we need
+
+  const FILENAME_KEYWORDS = [
+    "ID",
+    "TITLE",
+    "RES",
+    "AUTHOR",
+    "DATE",
+    "UP_DATE",
+    "DATE_TS",
+    "UP_DATE_TS",
+  ]
+  const RESOLUTIONS = ["Source", "540p", "360p"]
+
+  const autoEnabled = (0, external_Vue_namespaceObject.ref)(storage.get("auto_down_enabled"))
+  const filenameTemplate = (0, external_Vue_namespaceObject.ref)(storage.get("filename"))
+  const illegalCharReplacement = (0, external_Vue_namespaceObject.ref)(
+    storage.get("illegal_char_replacement")
+  )
+  const resolution = (0, external_Vue_namespaceObject.ref)(storage.get("preferred_res"))
+
+  const videoInfo = (0, external_Vue_namespaceObject.reactive)({
+    id: "",
+    title: "",
+    author: "",
+    created: 0,
+    size: 0,
+    error: "",
+  })
+  const sources = (0, external_Vue_namespaceObject.reactive)([])
+  const source = (0, external_Vue_namespaceObject.computed)(
+    () =>
+      sources.find((_ref) => {
+        let { label } = _ref
+        return label === resolution.value
+      }) || sources[0]
+  )
+
+  // indicates whether the sources belong to current page
+  const hasFreshSources = (0, external_Vue_namespaceObject.ref)(false)
+
+  const filename = (0, external_Vue_namespaceObject.computed)(() => {
+    try {
+      if (!source.value) throw "Please open a video"
+
+      return resolveFilename(filenameTemplate.value, source.value)
+    } catch (e) {
+      return `Unable to resolve filename (${formatError(e)})`
+    }
+  })
+
+  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
+    storage.set("preferred_res", resolution.value)
+  )
+  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
+    storage.set("filename", filenameTemplate.value)
+  )
+  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
+    storage.set("auto_down_enabled", autoEnabled.value)
+  )
+  ;(0, external_Vue_namespaceObject.watchEffect)(() =>
+    convertDownloadDropdown(void 0, autoEnabled.value)
+  )
+
+  function useDownloaderSettings() {
+    return {
+      FILENAME_KEYWORDS,
+      RESOLUTIONS,
+      autoDownEnabled: autoEnabled,
+      resolution,
+      filenameTemplate,
+      filenamePreview: filename,
+      illegalCharReplacement,
+    }
+  }
+
+  page("video", async (pageID, onLeave) => {
+    const videoActions = $(".page-video__actions").get(0)
+
+    if (!videoActions) {
+      log("Could not find video actions.")
+      return
+    }
+
+    onLeave(() => {
+      // prevent unexpected downloads
+      hasFreshSources.value = false
+    })
+
+    const $downloadButton = await cancelOnLeave(
+      onLeave,
+      until$(() => $(".page-video__actions .downloadButton"))
+    )
+
+    updateVideoInfo(videoActions)
+    updateSources($downloadButton.closest(".dropdown").get(0))
+
+    if (autoEnabled.value) convertDownloadDropdown($downloadButton.get(0), true)
+  })
+
+  function updateVideoInfo(videoActions) {
+    try {
+      // FIXME: reading the prop by a path is quite unreliable, any improvement?
+      const video = getReactEventHandlers(videoActions).children[1].props.video
+
+      videoInfo.id = video.id
+      videoInfo.title = video.title
+      videoInfo.created = new Date(video.createdAt).getTime()
+      videoInfo.author = video.user.name
+      videoInfo.size = video.file.size
+    } catch (e) {
+      log(e)
+      videoInfo.error = e + ""
+    }
+  }
+
+  function updateSources(downloadDropdown) {
+    const newSources = $(downloadDropdown)
+      .find(".dropdown__content a")
+      .map(function () {
+        const url = this.href
+        const label = this.innerText
+
+        return { url, label }
+      })
+      .get()
+
+    if (!newSources.length) return
+
+    sources.splice(0, sources.length, ...newSources)
+
+    hasFreshSources.value = true
+  }
+
+  function convertDownloadDropdown(downloadButton, enabled) {
+    const $button = downloadButton ? $(downloadButton) : $(".downloadButton")
+    const $dropdown = $button.closest(".dropdown")
+
+    if (!$dropdown.length) return
+
+    const rawButtonText = $button.text().replace(/\s*\(.*\)/, "")
+
+    if (enabled) {
+      if (!$dropdown.data("converted"))
+        $dropdown
+          .data("converted", true)
+          .on("click", function () {
+            download(this)
+          })
+          .children(".dropdown__content")
+          .css("display", "none")
+
+      const resolution = source.value?.label
+
+      $button.text(rawButtonText + (resolution ? ` (${resolution})` : ""))
+    } else {
+      $dropdown
+        .data("converted", false)
+        .off("click")
+        .children(".dropdown__content")
+        .css("display", "")
+      $button.text(rawButtonText)
+    }
+  }
+
+  function download(downloadDropdown) {
+    try {
+      if (!hasFreshSources.value) throw new Error("No sources found in current page.")
+      if (!source.value) throw new Error("Missing source.")
+
+      const $downloadButton = $(downloadDropdown).find(".downloadButton")
+
+      const filename = resolveFilename(filenameTemplate.value, source.value)
+
+      log("Downloading:", filename, source.value.url, GM_info.downloadMode)
+
+      if ("browser" === GM_info.downloadMode) {
+        setDownloadButtonEnabled(false)
+
+        if (false);
+
+        GM_download({
+          url: source.value.url,
+          name: filename,
+          onload: () => downloadEnded("onload"),
+          onerror: (e) => downloadEnded("onerror", e),
+          ontimeout: () => downloadEnded("ontimeout"),
+        })
+      } else {
+        const a = document.createElement("a")
+        a.href = source.value.url
+        a.download = filename
+        a.click()
+      }
+
+      function setDownloadButtonEnabled(enabled) {
+        if (enabled) {
+          // TODO: properly disable the button
+          $(downloadDropdown).css("pointer-events", "")
+          $downloadButton.css("background-color", "")
+        } else {
+          $(downloadDropdown).css("pointer-events", "none")
+          $downloadButton.css("background-color", "var(--primary-dark)")
+        }
+      }
+
+      function downloadEnded(type, e) {
+        setDownloadButtonEnabled(true)
+
+        if ("ontimeout" === type) e = { error: "timed_out" }
+
+        if (e && e.error) {
+          log(e)
+          printDownloadMessage(
+            `Download Error (${e.error}): ${(e.details && e.details.current) || "No info"}`
+          )
+        }
+      }
+    } catch (e) {
+      log(e)
+      printDownloadMessage(e + "")
+    }
+  }
+
+  function resolveFilename(template, source) {
+    if (videoInfo.error) throw new Error("Broken video info: " + videoInfo.error)
+
+    const replacements = {
+      ID: videoInfo.id,
+      TITLE: videoInfo.title,
+      RES: source.label,
+      AUTHOR: videoInfo.author,
+      DATE: formatDate(new Date(), ""),
+      DATE_TS: new Date().getTime() + "",
+      UP_DATE: formatDate(new Date(videoInfo.created), ""),
+      UP_DATE_TS: videoInfo.created + "",
+    }
+
+    const wrappedKeywords = FILENAME_KEYWORDS.map((k) => `{${k}}`)
+    const regex = new RegExp(`(${wrappedKeywords.join("|")})`, "g")
+
+    const basename = template.replace(regex, (match) => {
+      const keyword = match.slice(1, -1)
+      const value = replacements[keyword]
+
+      // remove path delimiters in keyword values
+      return value.replace(/[/\\]/g, illegalCharReplacement.value)
+    })
+
+    const ext = source.url.slice(source.url.lastIndexOf(".")).replace(/[^A-Za-z0-9.]/g, "")
+
+    const filename = basename + ext
+
+    return sanitizePath(filename, illegalCharReplacement.value)
+  }
+
+  function printDownloadMessage(msg) {
+    $(".page-video__bottom")
+      .css("flex-wrap", "wrap")
+      .append(`<div style="flex: 100% 0 0">${msg}</div>`)
   }
 
   const likeRateEnabled = (0, external_Vue_namespaceObject.ref)(storage.get("like_rates"))
